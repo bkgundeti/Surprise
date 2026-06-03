ecf6a996d7118d423e78064d179de3eb6e5cd143
Bhargavi Gundeti <bhargavigundeti01@gmail.com>
2026-06-03 18:04:33 +0530

    Fix allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['surprise-42mv.onrender.com']
+    allowedHosts: true
   }
 })
